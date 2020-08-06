bc0db60c01a5f7ef976281a2c18d52b6b1f15c51
toueileb <toueilebmohamed@gmail.com>
2020-08-06 19:28:29 +0200

    useEffect() with condition

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css';
-import ClassCounter from "./components/ClassCounter";
-import HookCounter from "./components/HookCounter";
-import HookCounterTwo from "./components/HookCounterTwo";
-import HookCounterThree from "./components/HookCounterThree";
 import HookCounterFour from "./components/HookCounterFour";
+import ClassCounterOne from "./components/ClassCounterOne";
+import HookCounterOne from "./components/HookCounterOne";
 
 function App() {
     return (
         <div className="App">
-            {/*<ClassCounter/>*/}
-            {/*<HookCounter />*/}
-            {/*<HookCounterTwo />*/}
-            {/*<HookCounterThree />*/}
-            <HookCounterFour />
+            {/*<HookCounterFour />*/}
+            {/*<ClassCounterOne />*/}
+            <HookCounterOne />
+            {/*<ClassCounterOne />*/}
         </div>
     );
 }
---
+++ src/components/HookCounterOne.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+
+function HookCounterOne(props) {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
+    useEffect(() => {
+        console.log("useEffect - Updating document title");
+        document.title = `Clicked ${count} times`;
+    },[count]);
+    return (
+        <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+            <button onClick={() => setCount(count + 1)}>Click {count} times</button>
+        </div>
+    );
+}
+
+export default HookCounterOne;
